dd2236e1f83fd5615faf19b4f26c4a0c5e827440
James <jbean46@gmail.com>
2017-06-29 15:20:00 +0200

    Refactor CALayer init with StyledPath.Composite

--- GraphicsTools/StyledPath.Composite+CALayer.swift
+++ GraphicsTools/StyledPath.Composite+CALayer.swift
@@ -12,18 +12,22 @@ extension CALayer {
     
     public convenience init(_ composite: StyledPath.Composite) {
 
-        func traverse(_ composite: StyledPath.Composite) {
+        func traverse(_ composite: StyledPath.Composite, building result: CALayer) -> CALayer {
             switch composite {
             case .leaf(let styledPath):
                 let layer = CAShapeLayer(styledPath)
-                addSublayer(layer)
-            case .branch(_, let trees):
-                // do something with group
-                trees.forEach(traverse)
+                return layer
+            case .branch(let group, let trees):
+                let layer = CALayer()
+                layer.frame = CGRect(group.frame)
+                trees.forEach {
+                    layer.addSublayer(traverse($0, building: layer))
+                }
+                return layer
             }
         }
         
         self.init()
-        traverse(composite)
+        self.addSublayer(traverse(composite, building: self))
     }
 }
