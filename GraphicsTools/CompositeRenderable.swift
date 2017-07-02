2145fac69b452ebf1f44d50de3a5d98c70b209d4
James <jbean46@gmail.com>
2017-07-02 22:20:33 +0200

    Prepare to size frame to fit contents for CompositeRenderable

--- GraphicsTools/CompositeRenderable.swift
+++ GraphicsTools/CompositeRenderable.swift
@@ -17,6 +17,9 @@ public protocol CompositeRenderable: Renderable {
 extension CompositeRenderable {
     
     public var rendered: StyledPath.Composite {
-        return .branch(StyledPath.Group("root"), components.map { $0.rendered })
+        let paths = components.map { $0.rendered }
+        let bbox = paths.flatMap { $0.leaves.map { $0.path.axisAlignedBoundingBox } }.sum
+        print("bbox: \(bbox)")
+        return .branch(StyledPath.Group("root"), paths)
     }
 }
